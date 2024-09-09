e20a2902d0931f39fc037982fb8751bc81959a20
Nathan Hughes <nathanhughes@MacBookPro.attlocal.net>
2024-09-09 12:15:24 -0400

    first and last commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nathan Hughes on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
